7441bd9aa79d208ddb2726e088a01e1136641d62
SOICHIRO-NISHIO-github <130431199+SOICHIRO-NISHIO-github@users.noreply.github.com>
2023-07-11 13:15:18 +0900

    Update yubs.rb

--- Formula/yubs.rb
+++ Formula/yubs.rb
@@ -2,9 +2,9 @@ VERSION="1.0.0"
 class Yubs < Formula
 desc "URL shortener via bit.ly, etc."
 homepage "https://github.com/SOICHIRO-NISHIO-github/yubs"
-url "https://github.com/SOICHIRO-NISHIO-github/yubs/releases/download/v#{VERSION}/yubs-#{VERSION}_darwin_amd64.tar.gz" 
+url "https://github.com/SOICHIRO-NISHIO-github/yubs/archive/refs/tags/v1.0.0.tar.gz" 
 version VERSION
-sha256 "c015ada80570a976a28e23e54db50de665c581883e9ffa8566a1b1fbd6003788"
+sha256 "e2c322f66226651641cd8dc207d2de9fdedb224a5909e3a501e6e9464d34c8f6"
 license "MIT"
 option "without-completions", "Disable bash completions"
 depends_on "bash-completion@2" => :optional
