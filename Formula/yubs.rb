b919af7ebd89faffece3d17dfe7b274a9a8ded52
SOICHIRO-NISHIO-github <130431199+SOICHIRO-NISHIO-github@users.noreply.github.com>
2023-07-11 12:14:22 +0900

    ファイル内容 yubs.rb

--- Formula/yubs.rb
+++ Formula/yubs.rb
@@ -5,12 +5,14 @@ homepage "https://github.com/SOICHIRO-NISHIO-github/yubs"
 url "https://github.com/SOICHIRO-NISHIO-github/yubs/releases/download/v#{VERSION}/yubs-#{VERSION}_darwin_amd64.tar.gz" 
 version VERSION
 sha256 "c015ada80570a976a28e23e54db50de665c581883e9ffa8566a1b1fbd6003788"
-license ”MIT"
+license "MIT"
 option "without-completions", "Disable bash completions"
 depends_on "bash-completion@2" => :optional
 def install
-    bin.install ”yubs"
-bash_completion.install "completions/bash/yubs" if build.with? "completions" end
+    bin.install "yubs"
+    bash_completion.install "completions/bash/yubs" if build.with? "completions" 
+end
   test do
-    system bin/”yubs", "--version"
-end end
+    system bin/"yubs", "--version"
+  end
+end
