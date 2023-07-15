dedef527f69f038f13a5eac3bcc6a2edf70a46a3
SOICHIRO-NISHIO-github <130431199+SOICHIRO-NISHIO-github@users.noreply.github.com>
2023-07-15 16:18:34 +0900

    yubs.rbの変更を行いました。

--- Formula/yubs.rb
+++ Formula/yubs.rb
@@ -1,16 +1,18 @@
-VERSION="1.0.0"
+VERSION="0.1.16"
 class Yubs < Formula
 desc "URL shortener via bit.ly, etc."
 homepage "https://github.com/SOICHIRO-NISHIO-github/yubs"
-url "https://github.com/SOICHIRO-NISHIO-github/yubs/archive/refs/tags/v1.0.0.tar.gz" 
+url "https://github.com/SOICHIRO-NISHIO-github/yubs/releases/download/v0.1.16/yubs-0.1.16_darwin_amd64.tar.gz" 
 version VERSION
-sha256 "e2c322f66226651641cd8dc207d2de9fdedb224a5909e3a501e6e9464d34c8f6"
+sha256 "9cf9b38cc53b085735905c17db0b39a91bd6e3de527962e670b0b5cf5f2aeac1"
 license "MIT"
 option "without-completions", "Disable bash completions"
 depends_on "bash-completion@2" => :optional
 def install
     bin.install "yubs"
-    bash_completion.install "completions/bash/yubs" if build.with? "completions" 
+    #bashの場合は、コメントアウトをはずし、zshをコメントアウトしてください。
+    #bash_completion.install "completions/bash/yubs" if build.with? "completions"
+    zsh_completion.install "completions/zsh/yubs" if build.with? "completions"
 end
   test do
     system bin/"yubs", "--version"
